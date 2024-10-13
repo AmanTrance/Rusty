a27479d292182f1fd9d760d8046dfbebc36a6532
AmanTrance <aman.hobo@gmail.com>
2024-10-14 03:07:55 +0530

    complete redis in/out

--- httpserver/server.go
+++ httpserver/server.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"time"
@@ -9,6 +13,10 @@ import (
 	_ "github.com/cespare/xxhash/v2"
 	_ "github.com/dgryski/go-rendezvous"
 	"github.com/redis/go-redis/v9"
+)
+
+const (
+	SECRET_KEY = "abcdefghijklmno1"
 )
 
 type Message struct {
@@ -19,9 +27,14 @@ type Message struct {
 }
 
 type ClientRequest struct {
-	Name string     `json:"name"`
-	Date *time.Time `json:"date"`
-	Id   int        `json:"id"`
+	Name string    `json:"name"`
+	Date time.Time `json:"date"`
+	Id   int       `json:"id"`
+}
+
+type GetRequestData struct {
+	From time.Time `json:"from"`
+	To   time.Time `json:"to"`
 }
 
 func main() {
@@ -55,17 +68,36 @@ outer:
 		select {
 		case msg := <-*channel:
 			if msg.request.Method != "POST" {
-				msg.response.WriteHeader(http.StatusBadRequest)
+				msg.response.WriteHeader(http.StatusMethodNotAllowed)
 				*msg.channel <- 0
 				break
 			}
 			var body ClientRequest
-			err := json.NewDecoder(msg.request.Body).Decode(&body)
-			if err != nil {
+			jsonErr := json.NewDecoder(msg.request.Body).Decode(&body)
+			if jsonErr != nil {
 				msg.response.WriteHeader(http.StatusBadRequest)
 				*msg.channel <- 0
 				break
 			}
+			byteData, MarshalErr := json.Marshal(body)
+			if MarshalErr != nil {
+				msg.response.WriteHeader(http.StatusInternalServerError)
+				*msg.channel <- 0
+				break
+			}
+			stringifyData, EncryptErr := EncryptData(byteData)
+			if EncryptErr != nil {
+				msg.response.WriteHeader(http.StatusInternalServerError)
+				*msg.channel <- 0
+				break
+			}
+			RedisErr := msg.client.RPush(ctx, "RandomData", stringifyData).Err()
+			if RedisErr != nil {
+				msg.response.WriteHeader(http.StatusInternalServerError)
+				*msg.channel <- 0
+				break
+			}
+			msg.response.WriteHeader(http.StatusOK)
 			*msg.channel <- 0
 			break
 		case <-ctx.Done():
@@ -80,10 +112,42 @@ outer:
 		select {
 		case msg := <-*channel:
 			if msg.request.Method != "GET" {
+				msg.response.WriteHeader(http.StatusMethodNotAllowed)
+				*msg.channel <- 0
+				break
+			}
+			data, err := msg.client.LRange(ctx, "RandomData", 0, -1).Result()
+			if err != nil {
+				msg.response.WriteHeader(http.StatusInternalServerError)
+				*msg.channel <- 0
+				break
+			}
+			var body GetRequestData
+			jsonError := json.NewDecoder(msg.request.Body).Decode(&body)
+			if jsonError != nil {
 				msg.response.WriteHeader(http.StatusBadRequest)
 				*msg.channel <- 0
 				break
 			}
+			for _, i := range data {
+				originalByteData, decryptError := DecryptData([]byte(i))
+				if decryptError != nil {
+					println(decryptError.Error())
+				}
+				reader := bytes.NewReader(originalByteData)
+				var clientData ClientRequest
+				unmarshalError := json.NewDecoder(reader).Decode(&clientData)
+				if unmarshalError != nil {
+					println(unmarshalError.Error())
+				}
+				if body.From.Compare(clientData.Date) == -1 && body.To.Compare(clientData.Date) == 1 {
+					println(clientData.Name, clientData.Id, clientData.Date.String())
+				} else {
+					continue
+				}
+			}
+			msg.response.WriteHeader(http.StatusOK)
+			*msg.channel <- 0
 			break
 		case <-ctx.Done():
 			break outer
@@ -106,4 +170,30 @@ func AddChannelAndClientToFunc(channel *chan Message, client *redis.Client) func
 			return
 		}
 	}
+}
+
+func EncryptData(data []byte) (string, error) {
+	block, err := aes.NewCipher([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	cfgEncrypter := cipher.NewCFBEncrypter(block, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
+	cipherText := make([]byte, len(data))
+	cfgEncrypter.XORKeyStream(cipherText, data)
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func DecryptData(data []byte) ([]byte, error) {
+	cipherText, err := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		return nil, err
+	}
+	block, cipherError := aes.NewCipher([]byte(SECRET_KEY))
+	if cipherError != nil {
+		return nil, cipherError
+	}
+	cfgDecrypter := cipher.NewCFBDecrypter(block, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
+	originalData := make([]byte, len(data))
+	cfgDecrypter.XORKeyStream(originalData, cipherText)
+	return originalData, nil
 }
